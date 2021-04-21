274bb552d198e7ec1bf8524b7082a77a64ce3388
Hrushikesh Badgujar <hrushibadgujar003@gmail.com>
2021-04-22 00:23:31 +0530

    hello folks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+        <h3>Hello, Folks </h3>
         <h3>Welcome, to my website</h3>
         <h1>Hrushikesh Badgujar</h1>
     </div>
