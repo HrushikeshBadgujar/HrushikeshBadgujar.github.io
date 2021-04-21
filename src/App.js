d8e20eb3daa07f8798d5489ff573d16201265003
Hrushikesh Badgujar <hrushibadgujar003@gmail.com>
2021-04-21 20:34:45 +0530

    deploy my site to github pages

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h3>Welcome, to my portfolio</h3>
+        <h1>Hrushikesh Badgujar</h1>
+    </div>
+  );
+}
+
+export default App;
